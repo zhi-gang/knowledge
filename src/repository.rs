e078c6e16fdc80f7e45e3ddc7b7a0de8fcde0e6f
Zhigang <zhi-gang.yan@outlook.com>
2024-02-01 15:32:33 +0800

    more api

--- src/repository.rs
+++ src/repository.rs
@@ -13,24 +13,85 @@ use chrono::Local;
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs;
+use std::path::Path;
 use tantivy::collector::TopDocs;
+use tantivy::doc;
 use tantivy::query::BooleanQuery;
 use tantivy::query::Query;
-use tantivy::IndexReader;
-// use std::io::BufRead;
-// use std::io::BufReader;
-use std::path::Path;
-use tantivy::doc;
 use tantivy::query::QueryParser;
 use tantivy::query_grammar::Occur;
 use tantivy::schema::*;
+use tantivy::time::format_description::well_known::Rfc3339;
 use tantivy::Index;
+use tantivy::IndexReader;
 use tantivy::ReloadPolicy;
+use tantivy::Searcher;
+use tantivy::TantivyError;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct KnownledgeDocument {
     title: String,
     body: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KnownledgeDocumentWithTime {
+    doc: KnownledgeDocument,
+    create_at: String,
+}
+
+impl KnownledgeDocumentWithTime {
+    fn pick_text_field(
+        retrieved_doc: &Document,
+        f: &Field,
+        field_name: &str,
+    ) -> tantivy::Result<String> {
+        match retrieved_doc.get_first(*f) {
+            Some(field_value) => match field_value {
+                Value::Str(text) => Ok(text.to_string()),
+                _ => Err(TantivyError::FieldNotFound(field_name.to_string())),
+            },
+            None => Err(TantivyError::FieldNotFound(field_name.to_string())),
+        }
+    }
+
+    fn pick_date_field(
+        retrieved_doc: &Document,
+        f: &Field,
+        field_name: &str,
+    ) -> tantivy::Result<String> {
+        match retrieved_doc.get_first(*f) {
+            Some(field_value) => match field_value {
+                Value::Date(ts) => Ok(ts.into_utc().format(&Rfc3339)?),
+                _ => Err(TantivyError::FieldNotFound(field_name.to_string())),
+            },
+            None => Err(TantivyError::FieldNotFound(field_name.to_string())),
+        }
+    }
+
+    pub fn build_from_document(
+        retrieved_doc: Document,
+        title: &Field,
+        body: &Field,
+        create_at: &Field,
+    ) -> tantivy::Result<Self> {
+        let title_str = Self::pick_text_field(&retrieved_doc, title, "title")?;
+        let body_str = Self::pick_text_field(&retrieved_doc, body, "body")?;
+        let create_at_str = Self::pick_date_field(&retrieved_doc, create_at, "create_at")?;
+        Ok(Self {
+            doc: KnownledgeDocument {
+                title: title_str,
+                body: body_str,
+            },
+            create_at: create_at_str,
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub enum Combiner {
+    AND,
+    OR,
 }
 
 /// The function that will create tantivy index in the path
@@ -48,12 +109,9 @@ pub struct KnownledgeDocument {
 ///
 /// # Returns:
 ///
-/// () or Error if index creation failed
-///
-
-///
-pub fn create_index(index_path: String) -> tantivy::Result<()> {
-    fs::remove_dir_all(&*index_path)?;
+/// (Index and Reader) or Error if index creation failed
+pub fn create_index(index_path: &str) -> tantivy::Result<(Index, IndexReader)> {
+    let _ = fs::remove_dir_all(&*index_path); //ignore error if directory does not exist
     fs::create_dir(&*index_path)?;
 
     let schema = make_schema();
@@ -62,7 +120,12 @@ pub fn create_index(index_path: String) -> tantivy::Result<()> {
     index
         .tokenizers()
         .register(CANG_JIE, CangJieTokenizer::default()); // Build cang-jie Tokenizer
-    Ok(())
+
+    let reader = index
+        .reader_builder()
+        .reload_policy(ReloadPolicy::OnCommit)
+        .try_into()?;
+    Ok((index, reader))
 }
 /// Load index from path
 /// It will register Cang-jie Tokenizer for Chinese characters
@@ -74,8 +137,7 @@ pub fn create_index(index_path: String) -> tantivy::Result<()> {
 /// # Returns:
 ///
 /// Index or error
-///
-pub fn load_index(index_path: String) -> tantivy::Result<(Index, IndexReader)> {
+pub fn load_index(index_path: &str) -> tantivy::Result<(Index, IndexReader)> {
     let index = Index::open_in_dir(index_path)?;
     index
         .tokenizers()
@@ -140,13 +202,6 @@ pub fn add_doc_in_batch(
     reader.reload()?; //refersh the reader;
     Ok(())
 }
-
-#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
-pub enum Combiner {
-    AND,
-    OR,
-}
-
 /// Query the documents for the given `keys` on Title and Body fields,
 /// Max `num` results.
 ///
@@ -164,53 +219,86 @@ pub enum Combiner {
 pub fn query_title_body(
     index: &Index,
     reader: &IndexReader,
-    keys: Vec<String>,
+    keys: Vec<&str>,
     op: Combiner,
     num: usize,
-) -> tantivy::Result<Vec<KnownledgeDocument>> {
+) -> tantivy::Result<Vec<KnownledgeDocumentWithTime>> {
     if keys.len() == 0 {
         return Ok(vec![]);
     }
 
     // reader.reload()?; //reload in udpate APIs
 
-    let searcher = reader.searcher();
+    let (title, body, create_at) = get_fields(&index)?;
 
-    let schema = index.schema();
-    let title = schema.get_field("title").unwrap();
-    let body = schema.get_field("body").unwrap();
     let query_parser = QueryParser::for_index(&index, vec![title, body]);
 
     let bool_query = build_bool_query(&query_parser, op, keys)?;
 
-    let top_docs = searcher.search(&bool_query, &TopDocs::with_limit(num))?;
+    let searcher = reader.searcher();
+    let top_docs: Vec<(f32, tantivy::DocAddress)> =
+        searcher.search(&bool_query, &TopDocs::with_limit(num))?;
+    build_results(&searcher, top_docs, num,  &title, &body, &create_at)
+}
+/// Query the documents for the given `key` on Title
+/// Max `num` results.
+///
+/// # Arguments
+///
+/// * `index` - The tantivy index to query.
+/// * `reader` - The global tantivy reader.
+/// * `title_str` - The search key to query with.
+/// * `num` - The maximum number of results to return.
+///
+/// # Returns
+///
+/// A vector of `KnownledgeDocument`s that match the search keys.
+pub fn query_title(
+    index: &Index,
+    reader: &IndexReader,
+    title_str: &str,
+    num: usize,
+) -> tantivy::Result<Vec<KnownledgeDocumentWithTime>> {
+    let (title, body, create_at) = get_fields(&index)?;
 
-    let mut result: Vec<KnownledgeDocument> = Vec::with_capacity(num);
+    let query_parser = QueryParser::for_index(&index, vec![title]);
+    let query = query_parser.parse_query(title_str)?;
+
+    let searcher = reader.searcher();
+    let top_docs = searcher.search(&query, &TopDocs::with_limit(num))?;
+    build_results(&searcher, top_docs, num,  &title, &body, &create_at)
+}
+fn build_results(
+    searcher: &Searcher,
+    top_docs: Vec<(f32, tantivy::DocAddress)>,
+    num: usize,
+    title: &Field,
+    body: &Field,
+    create_at: &Field,
+) -> tantivy::Result<Vec<KnownledgeDocumentWithTime>> {
+    let mut result: Vec<KnownledgeDocumentWithTime> = Vec::with_capacity(num);
     for (_score, doc_address) in top_docs {
         let retrieved_doc = searcher.doc(doc_address)?;
-        let title_str = retrieved_doc
-            .get_first(title)
-            .map(|field_value| match field_value {
-                Value::Str(text) => text.to_string(),
-                _ => String::new(), // Handle other value types as needed
-            })
-            .expect("could not find title in the document");
-
-        let body_str = retrieved_doc
-            .get_first(body)
-            .map(|field_value| match field_value {
-                Value::Str(text) => text.to_string(),
-                _ => String::new(), // Handle other value types as needed
-            })
-            .expect("could not find body in the document");
-        result.push(KnownledgeDocument {
-            title: title_str,
-            body: body_str,
-        });
+        let res = KnownledgeDocumentWithTime::build_from_document(
+            retrieved_doc,
+            &title,
+            &body,
+            &create_at,
+        )?;
+        result.push(res);
     }
-
     Ok(result)
 }
+
+fn get_fields(index: &Index) -> tantivy::Result<(Field, Field, Field)> {
+    let schema = index.schema();
+    let title = schema.get_field("title")?;
+    let body = schema.get_field("body")?;
+    let create_at = schema.get_field("create_at")?;
+
+    Ok((title, body, create_at))
+}
+
 
 /// Delete all documents in the repository
 pub fn delele_all(index: &Index, reader: &IndexReader) -> tantivy::Result<()> {
@@ -232,16 +320,21 @@ pub fn delele_all(index: &Index, reader: &IndexReader) -> tantivy::Result<()> {
 /// # Returns
 ///
 /// () or error
-pub fn delete<'a>(index: &Index, reader: &IndexReader, title_key: &'a str, ts:&'a str)->tantivy::Result<()> where{
+pub fn delete(
+    index: &Index,
+    reader: &IndexReader,
+    title_key: &str,
+    ts: &str,
+) -> tantivy::Result<()> where {
     let schema = index.schema();
     let title = schema.get_field("title").unwrap();
     let create_at = schema.get_field("create_at").unwrap();
 
     let query_parser_ts = QueryParser::for_index(&index, vec![create_at]);
-    let query_ts = query_parser_ts.parse_query(&*format!("\"create_at:\"{}\"",ts))?;
+    let query_ts = query_parser_ts.parse_query(&*format!("\"create_at:\"{}\"", ts))?;
     let query_parser_title = QueryParser::for_index(&index, vec![title]);
     let query_title = query_parser_title.parse_query(title_key)?;
-    let bool_query = BooleanQuery::new(vec![(Occur::Must,query_ts),(Occur::Must,query_title)]);
+    let bool_query = BooleanQuery::new(vec![(Occur::Must, query_ts), (Occur::Must, query_title)]);
 
     let mut index_writer = index.writer(10_000_000)?;
     index_writer.delete_query(Box::new(bool_query))?;
@@ -253,7 +346,7 @@ pub fn delete<'a>(index: &Index, reader: &IndexReader, title_key: &'a str, ts:&'
 fn build_bool_query(
     query_parser: &QueryParser,
     op: Combiner,
-    keys: Vec<String>,
+    keys: Vec<&str>,
 ) -> tantivy::Result<BooleanQuery> {
     let logic_op = match op {
         Combiner::AND => Occur::Must,
@@ -261,7 +354,6 @@ fn build_bool_query(
     };
 
     let mut all_query = Vec::<(Occur, Box<dyn Query>)>::with_capacity(keys.len());
-
     for key in keys {
         let query = query_parser.parse_query(&*key)?;
         all_query.push((logic_op, query));
@@ -269,11 +361,7 @@ fn build_bool_query(
     Ok(BooleanQuery::new(all_query))
 }
 
-fn make_doc<'a>(
-    index: &Index,
-    doc: &KnownledgeDocument,
-    now: &'a str,
-) -> tantivy::Result<Document> {
+fn make_doc(index: &Index, doc: &KnownledgeDocument, now: &str) -> tantivy::Result<Document> {
     let content = format!(
         "{{
         \"create_at\": \"{}\",
@@ -316,59 +404,78 @@ fn make_schema() -> Schema {
 
 /// Get tantivy formatted date of current local time
 fn now() -> String {
+    // Local::now().to_rfc3339()
     Local::now().format("%Y-%m-%dT%H:%M:%S%.3fZ").to_string()
+    // Local::now().format(Rfc3339).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::BufRead;
+    use std::io::BufReader;
 
     #[test]
     fn test_now() {
         let now = now();
-        assert!(now.len() == 24 );
+        println!("now : {}", now);
+        assert!(now.len() == 23);
         assert!(now.contains('-'));
         assert!(now.contains('T'));
         assert!(now.contains(':'));
         assert!(now.contains('.'));
     }
+
+    #[test]
+    fn create_repository() {
+        let begin = std::time::Instant::now();
+        let (index, index_reader) = create_index("index_test").unwrap();
+
+        let file = std::fs::File::open("data.json").unwrap();
+        let mut reader = BufReader::new(file);
+
+        let mut line = String::new();
+
+        let mut docs: Vec<KnownledgeDocument> = Vec::new();
+        loop {
+            if let Ok(n) = reader.read_line(&mut line) {
+                // println!("{},{}", n, line);
+                if n == 0 {
+                    break;
+                }
+
+                if let Ok(doc) = serde_json::from_str::<KnownledgeDocument>(&*line) {
+                    // add_doc(&index, data, &index_reader).unwrap(); //It cost expensively to commit the changes!!
+                    docs.push(doc);
+                }
+                line.clear();
+            }
+        }
+        add_doc_in_batch(&index, docs, &index_reader).unwrap();
+        let search = index_reader.searcher();
+        assert_eq!(9774, search.num_docs());
+        let end = std::time::Instant::now();
+        println!("create cost: {:?}", end.duration_since(begin));
+    }
+
+    #[test]
+    fn load_and_search() {
+        let begin = std::time::Instant::now();
+        let (index, reader) = load_index("index_test").unwrap();
+        let loaded = std::time::Instant::now();
+        let res =
+            query_title_body(&index, &reader, vec!["儿童", "头痛"], Combiner::AND, 10).unwrap();
+        let query = std::time::Instant::now();
+        println!("{:?}", res.get(0));
+        assert_eq!(10, res.len());
+        println!(
+            "load cost: {:?} ; query cost: {:?}",
+            loaded.duration_since(begin),
+            query.duration_since(loaded)
+        );
+
+        let res2 = query_title(&index, &reader, "湿气",10).unwrap();
+        println!("{:?}", res2.get(0));
+        assert_eq!(1, res2.len());
+    }
 }
-// fn add_documents(index: &Index, schema: &Schema) -> tantivy::Result<()> {
-//     let mut index_writer = index.writer(50_000_000)?;
-//     let title = schema.get_field("title")?;
-//     let body = schema.get_field("body")?;
-
-//     let file = std::fs::File::open("output.json")?;
-//     let mut reader = BufReader::new(file);
-
-//     let mut line = String::new();
-
-//     loop {
-//         if let Ok(n) = reader.read_line(&mut line) {
-//             println!("{},{}",n, line);
-//             if n == 0 {
-//                 break;
-//             }
-//             if let Ok(data) = serde_json::from_str::<Data>(&*line) {
-//                 println!("{}", data.title);
-//                 let _ = index_writer.add_document(doc!(
-//                     title => data.title,
-//                     body => data.body
-//                 ));
-//             }
-//             line.clear();
-//         }
-//     }
-//     index_writer.commit()?;
-//     Ok(())
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn test_setup() {
-//         setup("test_index".to_owned()).unwrap();
-//     }
-// }
